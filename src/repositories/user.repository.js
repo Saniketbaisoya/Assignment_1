d6d24c4f13be3858cd76d678fe62450ac403cde4
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-09-17 05:49:56 +0530

    adding the referral logic

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,7 +1,20 @@
 const User = require("../Schema/user.model.js");
+const AppError = require("../utlis/errors/error.js");
+const {StatusCodes} = require('http-status-codes');
 
-async function userCreate(data) {
+async function userCreate(data) { 
+    const existUser = await User.findOne({userName : data.userName});
+    if(existUser){
+        throw new AppError("The user is already exists !!",StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+    const referralInfo = await User.findOne({userName : data.referralUsername});
+    if(!referralInfo){
+        throw new AppError("The referral data is not present !!",StatusCodes.INTERNAL_SERVER_ERROR);
+    }
     const response = await User.create(data);
+
+    await referralInfo.referral.push(response._id);
+    await referralInfo.save();
     return response;
 }
 module.exports = {
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,11 +1,17 @@
-const { userRepository } = require("../repositories")
-
+const { userRepository } = require("../repositories");
+const AppError = require("../utlis/errors/error.js");
+const {StatusCodes} = require('http-status-codes');
 async function userRegister(data) {
+    /**
+     * if the user corresponding to the userName is not exists
+     * Then we create a new user...
+    */
     const payload = {
         userName : data.userName,
         password : data.password,
         referralUsername : data.referralUsername
     }
+    
     const user = await userRepository.userCreate(payload);
     return user;
 }
